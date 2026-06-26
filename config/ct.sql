0035a730ae34d59e5bef76ba7dae52c550902c25
geloxh <ace.blazado7@gmail.com>
2026-06-26 17:13:53 +0800

    Controllers

--- config/ct.sql
+++ config/ct.sql
@@ -1,3 +1,9 @@
+/**
+ * Kowd Database Structure
+ * Author: geloxh
+ * Description: This file contains the database structure for the Kowd platform.
+ */
+
 -- db name
 CREATE DATABASE IF NOT EXISTS kowd_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE kowd_db;
